07ea82581647781d4b3f5d70b5bcd3dba3126731
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2024-03-05 16:58:40 -0500

    Agregar archivos de proyecto.

---
+++ ContactManager/Controllers/ContactManager.cs
@@ -0,0 +1,30 @@
+﻿using ContactManager.MODELS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactManager : ControllerBase
+    {
+        [HttpGet]
+        public Contact[] Get()
+        {
+            return new Contact[]
+            {
+                new Contact
+                {
+                    ID = 1,
+                    Name ="David Sánchez"
+                },
+                new Contact
+                {
+                    ID= 2,
+                    Name = "Daniel Olivo"
+                }
+            };
+        }
+    }
+    
+}
---
+++ ContactManager/MODELS/Contact.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactManager.MODELS
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
